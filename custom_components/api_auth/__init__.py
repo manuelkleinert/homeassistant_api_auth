7150de243fef219058a6f2df22663bb6cb32b957
Manuel Kleinert <info@manuelkleinert.ch>
2026-03-23 09:02:18 +0100

    update

--- custom_components/api_auth/__init__.py
+++ custom_components/api_auth/__init__.py
@@ -1,7 +1,21 @@
-from .api import AuthView, TokenCheckView, LogoutView
+import os
+import json
+
+CONFIG_PATH = "/config"
+USERS_FILE = os.path.join(CONFIG_PATH, "api_users.json")
+TOKENS_FILE = os.path.join(CONFIG_PATH, "api_tokens.json")
+
+
+def ensure_files_exist():
+    if not os.path.exists(USERS_FILE):
+        with open(USERS_FILE, "w") as f:
+            json.dump([], f, indent=2)
+
+    if not os.path.exists(TOKENS_FILE):
+        with open(TOKENS_FILE, "w") as f:
+            json.dump({}, f, indent=2)
+
 
 async def async_setup(hass, config):
-    hass.http.register_view(AuthView)
-    hass.http.register_view(TokenCheckView)
-    hass.http.register_view(LogoutView)
+    ensure_files_exist()
     return True
--- custom_components/api_auth/api.py
+++ custom_components/api_auth/api.py
@@ -9,18 +9,6 @@ CONFIG_PATH = "/config"
 USERS_FILE = os.path.join(CONFIG_PATH, "api_users.json")
 TOKENS_FILE = os.path.join(CONFIG_PATH, "api_tokens.json")
 
-# ---------- Create Config Files if not exist ----------
-def ensure_files_exist():
-    if not os.path.exists(USERS_FILE):
-        with open(USERS_FILE, "w") as f:
-            json.dump([], f, indent=2)
-
-    if not os.path.exists(TOKENS_FILE):
-        with open(TOKENS_FILE, "w") as f:
-            json.dump({}, f, indent=2)
-
-ensure_files_exist()
-
 # ---------- Helpers ----------
 def load_json(path, default):
     if not os.path.exists(path):
